727899aaf445f9d1d0e71376470ac8b0e10a4a35
Mirkotorrisi <mirko.torrisi92@gmail.com>
2019-08-22 00:52:20 +0200

    Update Finance_App.java
    Implemented GUI using JavaFX

--- Finance_App.java
+++ Finance_App.java
@@ -1,135 +1,77 @@
-package Finance;
+package financeGUI;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.text.DecimalFormat;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+		static Scene main;
+		Stage window;
+		Button createNew;
+		Button show;
+		Button save;
+		static String file = "C:\\Users\\LuLele\\Desktop\\MortgagesInfo.csv";
+		static List<FinancialAccounts> accounts = new LinkedList<FinancialAccounts>();
 
-public class Finance_app {
-
-	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, DocumentException {
-		boolean bool = true;
-		Scanner np = new Scanner(System.in);
-		System.out.println("WELCOME\n\n");
-		List<FinancialAccounts> accounts = new LinkedList<FinancialAccounts>();
-		DecimalFormat f = new DecimalFormat("##.00");
-		String file = "C:\\User\\Desktop\\MortgagesInfo.csv";
-
-		List<String[]> newAccounts = utilities.CSV.read(file); 
-		for (String[]accountholders : newAccounts) {
-			String name = accountholders[0];
-			String dateStart = accountholders[1];
-			Integer mortgage = Integer.parseInt(accountholders[2]);
-			String mortType = accountholders[3].toUpperCase();
-			Integer numOfRepayments = Integer.parseInt(accountholders[4]);
-			String typeOfMortgage = accountholders[5].toLowerCase();
-			String riskProfile = accountholders[6];
-			if(riskProfile.toLowerCase().equals("high risk")) {
-				accounts.add(new HighRisk(name,dateStart,mortgage, mortType,numOfRepayments,typeOfMortgage));
-			}
-			else if (riskProfile.toLowerCase().equals("low risk")){
-				accounts.add(new LowRisk(name,dateStart,mortgage, mortType,numOfRepayments,typeOfMortgage));				
-			}
-			else {
-				System.out.println("Error reading risk profiling.");
-			}
-		}
-		while(bool == true) {
-		System.out.println("\n1 - CREATE NEW MORTGAGE\n2 - SHOW A MORTGAGE\n3 - SAVE AND QUIT");
-		int choice = np.nextInt();
-		np.nextLine();
-		
-		if(choice == 1) {
-			System.out.println("Insert name: ");
-			String name = np.nextLine();
-			System.out.println("Insert start date (yyyy-mm-dd): ");
-			String dateStart = np.nextLine();
-			System.out.println("Insert mortgage value: ");
-			int mortgage = np.nextInt();
-			np.nextLine();
-			System.out.println("Insert mortgage type (Italian, French): ");
-			String mortType = np.nextLine().toUpperCase();
-			System.out.println("Insert number of repayments: ");
-			int numOfRepayments = np.nextInt();
-			np.nextLine();
-			System.out.println("Insert type of repayments(Semestral, Trimestral, Bimestral, Monthly): ");
-			String typeOfMortgage = np.nextLine().toLowerCase();
-			System.out.println("Insert risk profile (1 - High, 2 - Low): ");
-			int choice2 = np.nextInt();
-			if(choice2 == 1) {
-				accounts.add(new HighRisk(name,dateStart,mortgage, mortType,numOfRepayments,typeOfMortgage));
-				System.out.println("High Risk Mortgage created for: "+name);
-			}
-			else {
-				accounts.add(new LowRisk(name,dateStart,mortgage, mortType,numOfRepayments,typeOfMortgage));
-				System.out.println("Low Risk Mortgage created for: "+name);
-			}			
-		}
-		else if(choice == 2) {
-			for(int i = 0; i < accounts.size(); i++) {
-				System.out.println("----------\nACCOUNT NUMBER: "+i);
-				accounts.get(i).adjustRate();
-				accounts.get(i).calcDates();
-				accounts.get(i).calculateMortgage();
-				accounts.get(i).showInfo();
-			}
-			System.out.println("SELECT ACCOUNT TO SHOW:");
-			int choice3 = np.nextInt();
-			accounts.get(choice3).showMort(accounts.get(choice3).calculateMortgage());
-		}
-		else if(choice == 3) {
-			System.out.println("\nHAVE A NICE DAY");
-			PrintWriter writer = new PrintWriter(file, "UTF-8");
-			for(int n = 0; n < accounts.size(); n++) {
-				writer.print(accounts.get(n).getName()+",");
-				writer.print(accounts.get(n).getDateStart()+",");
-				writer.print(accounts.get(n).mortgage+",");
-				writer.print(accounts.get(n).mortType+",");
-				writer.print(accounts.get(n).numOfRepayments+",");
-				writer.print(accounts.get(n).typeOfMortgage+",");
-				writer.println(accounts.get(n).riskProfile);
-				
-				Document document = new Document();
-				PdfWriter.getInstance(document, new FileOutputStream("C:\\User\\Desktop\\mortgages\\"+accounts.get(n).getName()+" Mortgage.pdf"));	 
-				document.open();
-
-
-				PdfPTable table = new PdfPTable(4);
-				table.addCell("DATE");
-				table.addCell("REPAYMENT");
-				table.addCell("CAPITAL QUOTE");
-				table.addCell("INTEREST QUOTE");
-
-				for(int i = 0; i < accounts.get(n).calculateMortgage()[0].length; i++) {
-				    accounts.get(n).adjustRate();
-				    table.addCell(accounts.get(n).calcDates()[i]);
-					table.addCell(f.format(accounts.get(n).calculateMortgage()[0][i]));
-				    table.addCell(f.format(accounts.get(n).calculateMortgage()[1][i]));
-				    table.addCell(f.format(accounts.get(n).calculateMortgage()[2][i]));
+		public static void main(String[] args) {
+			
+			//Creating List of Accounts, reading from CSV file
+			
+			List<String[]> newAccounts = utilities.CSV.read(file); 
+			for (String[]accountholders : newAccounts) {
+				String name = accountholders[0];
+				String dateStart = accountholders[1];
+				Integer mortgage = Integer.parseInt(accountholders[2]);
+				String mortType = accountholders[3].toUpperCase();
+				Integer numOfRepayments = Integer.parseInt(accountholders[4]);
+				String billing = accountholders[5].toLowerCase();
+				String riskProfile = accountholders[6];
+				if(riskProfile.equalsIgnoreCase("high risk")) {
+					accounts.add(new HighRisk(name,dateStart,mortgage, mortType,numOfRepayments,billing));
 				}
-				document.add(table);
-				Phrase phrase = new Phrase();
-				phrase.add("Name: "+ accounts.get(n).getName()+"\nMortgage: "+ accounts.get(n).mortgage+"$"); 
-				phrase.add("\nAmmortization: "+ accounts.get(n).mortType);
-				phrase.add("\n"+accounts.get(n).riskProfile + " profile");
-				document.add(phrase);
-				document.close();
+				else if (riskProfile.equalsIgnoreCase("low risk")){
+					accounts.add(new LowRisk(name,dateStart,mortgage, mortType,numOfRepayments,billing));				
+				}
+				else {
+					AlertBox.display("Error", "Error reading risk profiling.");
+				}
 			}
-			writer.close();
-			np.close();
-			bool = false;
+			launch(args);
+		}
+		public void start(Stage primaryStage) throws Exception {
+			//Main stage
+			window = primaryStage;
+			window.setTitle("Mortgages App");
+			createNew = new Button("Create New Mortgage");
+			createNew.setOnAction(e->form1.display());
+			show = new Button("Show Mortgages");
+			show.setOnAction(e->showMortgages.show(accounts));
+
+			save = new Button("Save PDFs");
+			save.setOnAction(e->Save.display(accounts));
+			VBox layout = new VBox(10);
+			Text welcome = new Text("-----Welcome-----");
+			layout.setPadding(new Insets(20,20,20,20));
+			layout.getChildren().addAll(welcome,createNew,show,save);
+			main = new Scene(layout, 200, 200);
+			window.setScene(main);
+			window.show();
+			}
+
+		public static void loadNewAccount(String name, String dateStart, int mortgage, String mortType, int numOfRepayments,
+			//Loading new accounts created with the application opened
+			String billing, String riskProfile) {
+			if(riskProfile.equalsIgnoreCase("high risk")) {
+				accounts.add(new HighRisk(name,dateStart,mortgage, mortType,numOfRepayments,billing));
+				}
+			else if (riskProfile.equalsIgnoreCase("low risk")){
+				accounts.add(new LowRisk(name,dateStart,mortgage, mortType,numOfRepayments,billing));				
+				}			
 			}
 		}
-	}
-}
-
