b12e218815c00701cb4f4c675b78742e385dd8e6
Mirkotorrisi <mirko.torrisi92@gmail.com>
2019-08-22 00:55:57 +0200

    Update FinancialAccounts.java

--- FinancialAccounts.java
+++ FinancialAccounts.java
@@ -1,8 +1,16 @@
-package Finance;
+package financeGUI;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public abstract class FinancialAccounts implements IBaseRate {
 	private String name;
@@ -12,45 +20,46 @@ public abstract class FinancialAccounts implements IBaseRate {
 	protected String mortType;
 	protected int numOfRepayments;
 	protected double repayment;
-	protected String typeOfMortgage;
+	protected String billing;
 	String riskProfile;
     DecimalFormat f = new DecimalFormat("##.00");
 
-	public FinancialAccounts(String name, String dateStart, int mortgage, String mortType, int numOfRepayments, String typeOfMortgage ) {
+	public FinancialAccounts(String name, String dateStart, int mortgage, String mortType, int numOfRepayments, String billing ) {
 		this.name = name;
 		this.dateStart = dateStart;
 		this.mortgage = mortgage;
 		this.numOfRepayments = numOfRepayments;
 		this.mortType = mortType;
-		this.typeOfMortgage = typeOfMortgage;
+		this.billing = billing;
 		setRate();
 	}
 	public abstract void setRate();
 	
 	protected String[] calcDates(){
+		//This method calculates each billing date
 		
 		String []dates = new String [numOfRepayments];
 	    LocalDate date = LocalDate.parse(dateStart);
 
-		if(typeOfMortgage.equals("semestral")) {
+		if(billing.equalsIgnoreCase("semestral")) {
 			for(int i = 0; i < numOfRepayments; i++) {
 			    date = date.plus(6, ChronoUnit.MONTHS);
 			    dates[i] = date.toString();
 				}
 		}
-		else if(typeOfMortgage.equals("trimestral")) {
+		else if(billing.equalsIgnoreCase("trimestral")) {
 			for(int i = 0; i < numOfRepayments; i++) {
 			    date = date.plus(3, ChronoUnit.MONTHS);
 			    dates[i] = date.toString();
 				}
 		}
-		else if(typeOfMortgage.equals("bimestral")) {
+		else if(billing.equalsIgnoreCase("bimestral")) {
 			for(int i = 0; i < numOfRepayments; i++) {
 			    date = date.plus(2, ChronoUnit.MONTHS);
 			    dates[i] = date.toString();
 				}
 		}
-		else if(typeOfMortgage.equals("monthly")) {
+		else if(billing.equalsIgnoreCase("monthly")) {
 			for(int i = 0; i < numOfRepayments; i++) {
 			    date = date.plus(1, ChronoUnit.MONTHS);
 			    dates[i] = date.toString();
@@ -59,44 +68,52 @@ public abstract class FinancialAccounts implements IBaseRate {
 		return dates;
 	}
 	protected void adjustRate() {
+		//Adjust interest rate converting from annual rate to fractionate rate
 		double fraction = 0;
-		if(typeOfMortgage.equals("semestral")) {
+		if(billing.equalsIgnoreCase("semestral")) {
 			fraction = (double) 1/2;
 			rate = Math.pow(1 + rate,fraction) - 1;
 		}
-		else if(typeOfMortgage.equals("trimestral")) {
+		else if(billing.equalsIgnoreCase("trimestral")) {
 			fraction = (double) 1/4;
 			rate = Math.pow(1 + rate,fraction) - 1;
 		}
-		else if(typeOfMortgage.equals("bimestral")) {
+		else if(billing.equalsIgnoreCase("bimestral")) {
 			fraction = (double) 1/6;
 			rate = Math.pow(1 + rate,fraction) - 1;
 		}
-		else if(typeOfMortgage.equals("monthly")) {
+		else if(billing.equalsIgnoreCase("monthly")) {
 			fraction = (double) 1/12;
 			rate = Math.pow(1 + rate,fraction) - 1;
 		}
 	}
 	public void showInfo() {
-		System.out.println("NAME: "+getName()+"\nMORTGAGE: "+getMortgage()
-							+"\nACCOUNT OPENED IN: "+dateStart+"\nRATE: "+rate+
-							 "\nMORTGAGE WILL END IN: "+calcDates()[numOfRepayments-1]+
-							"\nNUM OF REPAYMENTS: "+numOfRepayments+"\nFIRST REPAYMENT: "+f.format(calculateMortgage()[0][0])+
-							"\nLAST REPAYMENT: "+f.format(calculateMortgage()[0][numOfRepayments-1])+
-							"\nTYPE OF MORTGAGE: "+mortType+", "+typeOfMortgage);
+		AlertBox.displayTable("", "NAME: \nMORTGAGE: \nACCOUNT OPENED IN: \nRATE: \nMORTGAGE WILL END IN: \nNUM OF REPAYMENTS: \nFIRST REPAYMENT: \nLAST REPAYMENT: \nTYPE OF MORTGAGE: ",
+				getName()+"\n"
+				+getMortgage()+"\n"
+				+dateStart+"\n"
+				+"0"+f.format(rate)+"%\n"
+				+calcDates()[numOfRepayments-1]+"\n"
+				+numOfRepayments+"\n"
+				+f.format(calculateMortgage()[0][0])+"\n"
+				+f.format(calculateMortgage()[0][numOfRepayments-1])+"\n"
+				+mortType+", "+billing);
 	}
 	protected void showMort(double[][] quotes) {
+		String mortView = "";
 		for(int i = 0; i < quotes[0].length; i++) {
-			System.out.println("\nRepayment: "+f.format(quotes[0][i])+"\nCapital quote: "+f.format(quotes[1][i])+
-					"\nInterest quote: "+f.format(quotes[2][i])+"\nDATE: "+calcDates()[i]);
+			mortView += "\nRepayment: "+f.format(quotes[0][i])+"\nCapital quote: "+f.format(quotes[1][i])+
+					"\nInterest quote: "+f.format(quotes[2][i])+"\nDATE: "+calcDates()[i]+"\n";
 			}
+		AlertBox.displayScrollbar(getName(), mortView);
 	}
 	protected double[][] calculateMortgage() {
+		//Method to calculate mortgages, french or italian
 		double[][] quotes = new double[3][numOfRepayments];
 		double capitalLeft = mortgage;
 		double Interests = 0;
 		double capitalQuote1 = 0;
-		if (mortType.equals("ITALIAN")){
+		if (mortType.equalsIgnoreCase("Italian")){
 			capitalQuote1 = mortgage/numOfRepayments;
 			for(int n = 0; n < numOfRepayments; n++) {
 				Interests = capitalLeft*(rate);
@@ -107,7 +124,7 @@ public abstract class FinancialAccounts implements IBaseRate {
 				quotes[2][n] = Interests;
 				}
 		}
-		else if(mortType.toUpperCase().equals("FRENCH")){
+		else if(mortType.toUpperCase().equalsIgnoreCase("French")){
 			repayment = mortgage/((1-(Math.pow(1+rate, -numOfRepayments)))/rate);
 			for(int n = 0; n < numOfRepayments;n++) {
 				Interests = capitalLeft * rate;
@@ -119,6 +136,33 @@ public abstract class FinancialAccounts implements IBaseRate {
 				}
 		}
 		return quotes;
+	}
+	public void savePdf(double[][] quotes) throws FileNotFoundException, DocumentException {
+		//Method to save pdf for each mortgage selected. Uses Pdf Writer
+		Document document = new Document();
+		PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\LuLele\\Desktop\\mortgages\\"+getName()+" Mortgage.pdf"));	 
+		document.open();
+
+		PdfPTable table = new PdfPTable(4);
+		table.addCell("DATE");
+		table.addCell("REPAYMENT");
+		table.addCell("CAPITAL QUOTE");
+		table.addCell("INTEREST QUOTE");
+
+		for(int i = 0; i < quotes[0].length; i++) {
+			adjustRate();
+		    table.addCell(calcDates()[i]);
+			table.addCell(f.format(quotes[0][i]));
+		    table.addCell(f.format(quotes[1][i]));
+		    table.addCell(f.format(quotes[2][i]));
+			}
+		document.add(table);
+		Phrase phrase = new Phrase();
+		phrase.add("Name: "+ getName()+"\nMortgage: "+ mortgage+"$"); 
+		phrase.add("\nAmmortization: "+ mortType);
+		phrase.add("\n"+riskProfile + " profile");
+		document.add(phrase);
+		document.close();
 	}
 	protected int getMortgage() {
 		return mortgage;
